5b6d736e2de97a4772ed596a5c0182b57ec1c3c4
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-09-02 22:19:55 +0800

    fixed package.json
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- demo/polymer-element.js
+++ demo/polymer-element.js
@@ -1,16 +1,16 @@
-class CustomElement extends PolymerElement {
-  static get properties () {
-    return {
-      prop1: {
-        type: String,
-        value: 'World'
-      }
-    };
-  }
+// class CustomElement extends PolymerElement {
+//   static get properties () {
+//     return {
+//       prop1: {
+//         type: String,
+//         value: 'World'
+//       }
+//     };
+//   }
 
-  static get template () {
-    return html`Hello [[prop1]]`;
-  }
-}
+//   static get template () {
+//     return html`Hello [[prop1]]`;
+//   }
+// }
 
-window.customElements.define('custom-element', CustomElement);
+// window.customElements.define('custom-element', CustomElement);
